abf0e739693a25c5ca108a9eb27f7d346230bcfe
Andrei Leon Salas <andreileonsalas@gmail.com>
2020-11-24 21:38:54 -0600

    preprocesador v3

---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define NAME "Andrei Leon Salas"
+#define AGE 10
+void xprint()
+{
+    printf("Hello World!");
+	printf("%s is over %d years old.\n", NAME, AGE);
+}
